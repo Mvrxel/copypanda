de382564229b55624be4eb1a1eab68b9b2f9a5ad
Marcel Bilski <mvrxel@gmail.com>
2025-03-17 13:13:45 +0100

    feat: Google auth workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,26 @@ import Link from "next/link";
 import { auth } from "@/server/auth";
 import { HydrateClient } from "@/trpc/server";
 import { Button } from "../components/ui/button";
-
+import { SignOutButton } from "./_components/sign-out-button";
 export default async function Home() {
   const session = await auth();
   return (
     <HydrateClient>
       {session ? (
         // Content for authenticated users
-        <div>{session.user.email}</div>
+        <div>
+          {session.user.email}
+          <div>
+            <SignOutButton />
+          </div>
+        </div>
       ) : (
         // Content for unauthenticated users
         <div className="flex h-screen w-full items-center justify-center">
           <div>
             <h2 className="mb-4 text-2xl font-bold">Copy Panda</h2>
             <p className="mb-6">Copy Panda is currently in private alpha.</p>
-            <Link href="/sign-in">
+            <Link href="/auth/sign-in">
               <Button className="w-full">Sign in</Button>
             </Link>
           </div>
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -31,7 +31,7 @@ declare module "next-auth" {
   //   // role: UserRole;
   // }
 }
-const ALLOWED_EMAILS = ["marcel.bilski@anzuro.com", "mvrxel@gmail.com"];
+const ALLOWED_EMAILS = ["mvrxel@gmail.com"];
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
  *
